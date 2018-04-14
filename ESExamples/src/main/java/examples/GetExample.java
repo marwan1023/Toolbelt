4fcd598c2c32223384d8cb3555a46d2b42440791
johnson_marcus <darthbison@gmail.com>
2018-04-14 17:27:29 -0400

    Examples round 3

---
+++ ESExamples/src/main/java/examples/GetExample.java
@@ -0,0 +1,65 @@
+package examples;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.index.VersionType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class GetExample {
+
+    public static void main(String[] args) {
+
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+
+        GetRequest request = new GetRequest(
+                "posts",
+                "doc",
+                "1");
+
+        String[] includes = new String[]{"message", "*Date"};
+        String[] excludes = Strings.EMPTY_ARRAY;
+        FetchSourceContext fetchSourceContext = new FetchSourceContext(true, includes, excludes);
+        request.fetchSourceContext(fetchSourceContext);
+
+        request.storedFields("message");
+        request.routing("routing");
+        request.parent("parent");
+        request.preference("preference");
+        request.realtime(false);
+        request.refresh(true);
+        request.version(2);
+        request.versionType(VersionType.EXTERNAL);
+
+        try {
+            GetResponse getResponse = client.get(request);
+            String message = getResponse.getField("message").getValue();
+            String index = getResponse.getIndex();
+            String type = getResponse.getType();
+            String id = getResponse.getId();
+            if (getResponse.isExists()) {
+                long version = getResponse.getVersion();
+                String sourceAsString = getResponse.getSourceAsString();
+                Map<String, Object> sourceAsMap = getResponse.getSourceAsMap();
+                byte[] sourceAsBytes = getResponse.getSourceAsBytes();
+            } else {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
