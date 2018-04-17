06c6ab5eefe56f37bb78543f812e4a1a2633ac2d
darthbison <darthbison@gmail.com>
2018-04-17 17:29:00 -0400

    Update index.js

--- sensor-project/server/index.js
+++ sensor-project/server/index.js
@@ -6,22 +6,12 @@ const app = express()
 
     app.get('/temperature', function (req, res) 
             {      
-               sensor.read(11, 4, function (err, temperature,     humidity) 
-                           {        
-                        if (!err) 
-                            {    
-                              res.send(temperature.toFixed(1) + '°C');     
-                            }   
-               }); 
+               getSensorReadings((err, temperature, humidity) => {        if (!err) {          res.send(temperature.toFixed(1) + '°C')        }      })
     });   
 
    app.get('/humidity', function (req, res)
            {    
-     sensor.read(11, 4, function (err, temperature,     humidity) {  
-       if (!err) {   
-         res.send(humidity.toFixed(1) + '%');    
-       }    
-     });  
+     getSensorReadings((err, temperature, humidity) => {        if (!err) {          res.send(humidity.toFixed(1) + '%')        }      })
    });  
 
 app.listen(3000,"0.0.0.0", function(){       console.log('Server listening on port 3000');     });
