75af7d56c7fdf08b8cdc04524ab82b39a11ac057
darthbison <darthbison@gmail.com>
2018-04-17 17:14:43 -0400

    Update index.js with sensor readings

--- sensor-project/server/index.js
+++ sensor-project/server/index.js
@@ -1,16 +1,27 @@
 const chalk = require('chalk');
 const express = require('express');
+const sensor = require('node-dht-sensor');
 
 const app = express()
 
-app.get('/temperature', function(req, res) {      res.send('24 °C');    });
+    app.get('/temperature', function (req, res) 
+            {      
+               sensor.read(11, 4, function (err, temperature,     humidity) 
+                           {        
+                        if (!err) 
+                            {    
+                              res.send(temperature.toFixed(1) + '°C');     
+                            }   
+               }); 
+    });   
 
-app.get('/humidity', function(req, res) {      res.send('48%');    });
-
-
-//const port = 3000;
-//Run Server
-//app.listen(process.env.PORT || port, '0.0.0.0', () => console.log(chalk.blue(`Listening intently on port ${port}`)));
-
+   app.get('/humidity', function (req, res)
+           {    
+     sensor.read(11, 4, function (err, temperature,     humidity) {  
+       if (!err) {   
+         res.send(humidity.toFixed(1) + '%');    
+       }    
+     });  
+   });  
 
 app.listen(3000,"0.0.0.0", function(){       console.log('Server listening on port 3000');     });
