b2f2c6491b2f86096ed799a0eb5549992a3bf4cc
johnson_marcus <darthbison@gmail.com>
2018-04-19 16:05:07 -0400

    reformatting

--- sensor-project/server/index.js
+++ sensor-project/server/index.js
@@ -1,7 +1,18 @@
-   const express = require('express')         const path = require('path')         const app = express()         const getCachedSensorReadings = require('./get-     cached-         sensor-readings')        app.get('/temperature', function (req, res) {           res.send('<strong>' +          getCachedSensorReadings.getTemperature()    .toFixed(1) +           '</strong>' + '°C')        })          app.get('/humidity', function (req, res) {      res.send('<strong>' +      getCachedSensorReadings.getHumidity().toFixed(1) +       '</strong>' + '%')    })        
+   const express = require('express')
+   const path = require('path')
+   const app = express()
+   const getCachedSensorReadings = require('./get-cached-sensor-readings')
 
-app.use('/public', express.static(path.join(__dirname,     'public')))       
+   app.get('/temperature', function(req, res) {
+       res.send('<strong>' + getCachedSensorReadings.getTemperature().toFixed(1) + '</strong>' + '°C')
+   })
 
-app.listen(3000, "0.0.0.0", function () {          console.log('Server listening on port 3000')        })
+   app.get('/humidity', function(req, res) {
+       res.send('<strong>' + getCachedSensorReadings.getHumidity().toFixed(1) + '</strong>' + '%')
+   })
 
+   app.use('/public', express.static(path.join(__dirname, 'public')))
 
+   app.listen(3000, "0.0.0.0", function() {
+       console.log('Server listening on port 3000')
+   })
