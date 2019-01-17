4d54c3d35380f2eb9277a972b24e65c8534e68c1
johnson_marcus <darthbison@gmail.com>
2019-01-17 09:03:59 -0500

    EnvirohiveGraph

---
+++ Java/EnvirohiveGraph/src/main/java/hive/SensorDeserializer.java
@@ -0,0 +1,43 @@
+package hive;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import hive.Sensor;
+
+import java.io.IOException;
+
+public class SensorDeserializer extends StdDeserializer<Sensor> {
+
+    public SensorDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public Sensor deserialize(JsonParser parser, DeserializationContext deserializer) throws IOException {
+        Sensor sensor = new Sensor();
+        while(!parser.isClosed()){
+            JsonToken jsonToken = parser.nextToken();
+
+            if(JsonToken.FIELD_NAME.equals(jsonToken)){
+                String fieldName = parser.getCurrentName();
+
+
+                parser.nextToken();
+
+                if("temperature".equals(fieldName)){
+                    sensor.setTemperature(parser.getValueAsString());
+                } else if ("humidity".equals(fieldName)){
+                    sensor.setHumidity(parser.getValueAsString());
+                } else if ("pressure".equals(fieldName)) {
+                    sensor.setPressure(parser.getValueAsString());
+                }
+                else if ("host".equals(fieldName)) {
+                    sensor.setHost(parser.getValueAsString());
+                }
+            }
+        }
+        return sensor;
+    }
+}
---
+++ Java/EnvirohiveGraph/src/main/java/playcode/JavaFXApplication250.java
@@ -0,0 +1,82 @@
+package playcode;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeMap;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.control.Tooltip;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author blj0011
+ */
+public class JavaFXApplication250 extends Application
+{
+
+    @Override
+    public void start(Stage stage)
+    {
+        stage.setTitle("Line Chart Sample");
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series<Number, Number> series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        Random rand = new Random();
+
+        TreeMap<Integer, Integer> data = new TreeMap();
+        //Create Chart data
+        for (int i = 0; i < 3; i++) {
+            data.put(rand.nextInt(51), rand.nextInt(51));
+        }
+        Set set = data.entrySet();
+
+        Iterator i = set.iterator();
+        while (i.hasNext()) {
+            Map.Entry me = (Map.Entry) i.next();
+            System.out.println(me.getKey() + " - " + me.getValue());
+            series.getData().add(new XYChart.Data(me.getKey(), me.getValue()));//Add data to series
+        }
+
+        lineChart.getData().add(series);
+
+        //loop through data and add tooltip
+        //THIS MUST BE DONE AFTER ADDING THE DATA TO THE CHART!
+        for (Data<Number, Number> entry : series.getData()) {
+            entry.getNode().setOnMouseEntered(mousePressed -> {
+                System.out.println("Entered!");
+                Tooltip t = new Tooltip(entry.getYValue().toString());
+                Tooltip.install(entry.getNode(), t);
+            });
+        }
+
+        Scene scene = new Scene(lineChart, 800, 600);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+}
