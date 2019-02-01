3d229180d2da8e138feaa36532ba705686a8415b
darthbison <darthbison@gmail.com>
2019-02-01 18:24:38 -0500

    CV Example updates

--- Python/opencv/examples/Chapter01/05_image_color_merge.py
+++ Python/opencv/examples/Chapter01/05_image_color_merge.py
@@ -5,9 +5,11 @@ img = cv2.imread('./images/input.jpg', cv2.IMREAD_COLOR)
 g,b,r = cv2.split(img)
 gbr_img = cv2.merge((g,b,r))
 rbr_img = cv2.merge((r,b,r))
+bbr_img = cv2.merge((b,b,g))
 
 cv2.imshow('Original', img)
 cv2.imshow('GRB', gbr_img)
 cv2.imshow('RBR', rbr_img)
+cv2.imshow('BBR', bbr_img)
 
 cv2.waitKey()
