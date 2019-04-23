38d2df137dd14bd6d52d6058d66b005b5245b972
Marcus Johnson <darthbison@gmail.com>
2019-04-23 05:10:52 -0400

    Updating the Art Frame with Paint App

--- Java/Playground/src/main/java/huplay/Paint.java
+++ Java/Playground/src/main/java/huplay/Paint.java
@@ -1,44 +1,33 @@
 package huplay;
 
-import java.awt.image.RenderedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Stack;
-import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
+import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
 import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Stack;
 
-public class Paint extends Application {
+public class Paint {
 
-    @Override
-    public void start(Stage primaryStage) {
+    public Scene getPaintScene(Stage primaryStage) {
         Stack<Shape> undoHistory = new Stack();
         Stack<Shape> redoHistory = new Stack();
 
@@ -425,12 +414,14 @@ public class Paint extends Application {
 
         Scene scene = new Scene(pane, 1200, 800);
 
-        primaryStage.setTitle("Paint");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            System.out.println("Stage is closing");
+            // Save file
+        });
+
+
+        return scene;
+
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
