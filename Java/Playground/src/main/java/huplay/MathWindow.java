9c1f51c5689926d98e92071ec10ab89d8dba2fae
darthbison <darthbison@gmail.com>
2019-04-26 11:33:14 -0400

    Update MathWindow.java

--- Java/Playground/src/main/java/huplay/MathWindow.java
+++ Java/Playground/src/main/java/huplay/MathWindow.java
@@ -4,7 +4,46 @@ import javax.swing.*;
 
 public abstract class MathWindow extends JFrame {
 
+    JTextField plusbox = new JTextField();
+    JTextField xbox = new JTextField();
+    JTextField ybox = new JTextField();
+    JTextField zbox = new JTextField();
+    JButton enterbutton = new JButton();
+    JTextField diologebox = new JTextField();
+    JTextField scorebox = new JTextField();
+    JLabel JLabel1 = new JLabel();
 
+    JLabel equalsign = new JLabel();
+    JTextField f = new JTextField();
+    JTextField answbox = new JTextField();
+    JButton answer = new JButton();
+
+    JLabel JLabel2 = new JLabel();
+    JButton nameEntryButton = new JButton();
+    JButton JButton2 = new JButton();
+    JButton JButton3 = new JButton();
+
+    ImagePanel imagePanel1 = new ImagePanel();
+    ImagePanel imagePanel2 = new ImagePanel();
+
+    JLabel JLabel3 = new JLabel();
+    JLabel JLabel4 = new JLabel();
+
+    ImagePanel imagePanel3 = new ImagePanel();
+    ImagePanel imagePanel4 = new ImagePanel();
+
+    // Image Viewers
+    ImagePanel imageViewer1 = new ImagePanel();
+    ImagePanel imageViewer2 = new ImagePanel();
+    ImagePanel imageViewer3 = new ImagePanel();
+    ImagePanel imageViewer4 = new ImagePanel();
+    ImagePanel imageViewer5 = new ImagePanel();
+    ImagePanel imageViewer6 = new ImagePanel();
+    ImagePanel imageViewer7 = new ImagePanel();
+    ImagePanel imageViewer8 = new ImagePanel();
+    ImagePanel imageViewer9 = new ImagePanel();
+    ImagePanel imageViewer10 = new ImagePanel();
+    ImagePanel imageViewer11 = new ImagePanel();
 
 
 }
