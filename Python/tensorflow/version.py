ba3e9afb2ba22d6f92656839dd8e82d7eca0947b
Marcus Johnson <darthbison@gmail.com>
2019-09-14 09:06:27 -0400

    Add TensorFlow

---
+++ Python/tensorflow/version.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
+
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
